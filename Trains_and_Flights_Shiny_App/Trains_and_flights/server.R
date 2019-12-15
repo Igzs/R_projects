75f6da3791351c1219ce773aae2baf26440ebe98
Igor Fidalgo <igorfidalgo2@gmail.com>
2019-12-15 02:20:03 +0100

    Added disruption visualisation

--- Trains_and_Flights_Shiny_App/Trains_and_flights/server.R
+++ Trains_and_Flights_Shiny_App/Trains_and_flights/server.R
@@ -12,20 +12,52 @@ library(dplyr)
 library(ggplot2)
 library(scales)
 library(ggthemes)
+library(reshape2)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     full_trains_df <- read.csv("datasets/full_trains.csv")
-    output$testplot <- renderPlot({
+    output$carried_bplot <- renderPlot({
         choice = sym(input$choice)
+      
         # generate bins based on input$bins from ui.R
-        test_df <- full_trains_df %>% group_by(!!(choice)) %>% summarize(total = sum(total_num_trips) - sum(num_of_canceled_trains))
-        print(test_df)
-        ggplot(test_df,aes(x=!!(choice),y=total)) +
-          geom_bar(stat="identity",width = 0.5,fill="darkblue") +
+        carried_df <- full_trains_df %>% group_by(!!(choice)) %>% summarize(total = sum(total_num_trips) - sum(num_of_canceled_trains))
+        ggplot(carried_df,aes(x=!!(choice),y=total)) +
+          geom_bar(stat="identity",width = 0.5,fill="dodgerblue") +
           scale_y_continuous(labels = comma) +
           scale_fill_manual(values=c("#56B4E9")) +
-          theme(axis.text.x = element_text(angle = 45, hjust = 1))
+          ggtitle(paste("Total number of carried train rides by",choice)) +
+          theme(axis.text.x = element_text(angle = 45, hjust = 1, size=10),
+                plot.title = element_text(color="black", size=24, face="bold")
+                )
 
+    })
+    
+    output$multiple_bplot <- renderPlot({
+      choice = sym(input$choice)
+      
+      #total number of train rides delayed at departure
+  
+      total_delay_dep <- full_trains_df %>% group_by(!!(choice)) %>% summarize(total_delay_dep = sum(num_late_at_departure))
+      #total number of train rides delayed at arrival
+      
+      total_delay_arr <- full_trains_df %>% group_by(!!(choice)) %>% summarize(total_delay_arr = sum(num_arriving_late, na.rm = TRUE))
+      #total number of canceled trains
+    
+      total_canceled <- full_trains_df %>% group_by(!!(choice)) %>% summarize(total_canceled = sum(num_of_canceled_trains))
+      #combine the three previous dataframes
+      combine_df <- melt(cbind(total_canceled,total_delay_dep,total_delay_arr), id.vars = c(choice))
+      
+    
+      ggplot(combine_df, aes(x=!!(choice),y=value,fill=variable)) + 
+        geom_bar(stat="identity", width=0.4,position="dodge") +
+        scale_y_continuous(labels = comma) +
+        labs(x=choice,y="Total") +
+        ggtitle(paste("Total disruption by",choice)) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1, size=10),
+              plot.title = element_text(color="black", size=24, face="bold")
+              ) 
+      
+      
     })
 
 })
--- Trains_and_Flights_Shiny_App/Trains_and_flights/ui.R
+++ Trains_and_Flights_Shiny_App/Trains_and_flights/ui.R
@@ -11,52 +11,54 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(navbarPage("Data Analytics Project",
-                   
-                   #Documentation tab
-                   tabPanel("Documentation",
-                            h1("Data Analytics Final Project"),
-                            p("Welcome to the Trains and Flights Shiny App"),
-                            h2("Goal of the project:"),
-                            p("The goal of this project is to construct dashboards displaying information about SNCF rail trafic and US flights.",br(),
-                              "This project is the synthesis of every concept reviewed during this Data Analytics course."), 
-                            h2("How to use this shiny application:"),
-                            p("Navigate through the application using the navigation bar.",br(),
-                              "Interact with the different visualizations using the graphical interface."),
-                            h2("About the datasets:"),
-                            h3("SNCF rail trafic details"),
-                            p("The dataset associated contains aggregated information about rail trafic accross 4 years ",br(),
-                              " At issue is whether the data show signs of improvement or degradation of the state of french rail trafic.",br(),
-                              " There were 5462 observations accross 59 train stations. Information about departure and arrival delays, delay causes and average duration were recorded for all thoses lines",br()
-                            ),
-                            h3("US flights details"),
-                            p("Three datasets were used to construct this dashboard:",br()),
-                            tags$ol(
-                              tags$li("Airlines: Maps IATA flight codes to a specific airline."),
-                              tags$li("Airports: Maps IATA flight codes to airports geographical information"),
-                              tags$li("Flights: Contains an overview of all the information about the flights")
+                           
+                           #Documentation tab
+                           tabPanel("Documentation",
+                                    h1("Data Analytics Final Project"),
+                                    p("Welcome to the Trains and Flights Shiny App"),
+                                    h2("Goal of the project:"),
+                                    p("The goal of this project is to construct dashboards displaying information about SNCF rail trafic and US flights.",br(),
+                                      "This project is the synthesis of every concept reviewed during this Data Analytics course."), 
+                                    h2("How to use this shiny application:"),
+                                    p("Navigate through the application using the navigation bar.",br(),
+                                      "Interact with the different visualizations using the graphical interface."),
+                                    h2("About the datasets:"),
+                                    h3("SNCF rail trafic details"),
+                                    p("The dataset associated contains aggregated information about rail trafic accross 4 years ",br(),
+                                      " At issue is whether the data show signs of improvement or degradation of the state of french rail trafic.",br(),
+                                      " There were 5462 observations accross 59 train stations. Information about departure and arrival delays, delay causes and average duration were recorded for all thoses lines",br()
                                     ),
-                              p("The combination of these datasets allows us to have an overview of the state of US air trafic",br(),
-                              "Comparisons can be drawn between the two dashboards to reveal interesting results "
-                             )
-                      
-                          ),
-                   tabPanel("SNCF dashboard",
-                            
-                              fluidRow(column(width=2,
-                                       wellPanel(
-                                         radioButtons("choice", "Choose feature to aggregate on:",
-                                                      c("Year" = "year",
-                                                        "Departure Station" = "departure_station"
-                                                        ))
-                                     )
-                              
-                                 ),
-                                column(width=8,
-                                       titlePanel(h1("Total number of carried train rides",align="center")),
-                                       plotOutput('testplot')
-                              
-                          )
-                      )
-                   )
-    )
+                                    h3("US flights details"),
+                                    p("Three datasets were used to construct this dashboard:",br()),
+                                    tags$ol(
+                                      tags$li("Airlines: Maps IATA flight codes to a specific airline."),
+                                      tags$li("Airports: Maps IATA flight codes to airports geographical information"),
+                                      tags$li("Flights: Contains an overview of all the information about the flights")
+                                    ),
+                                    p("The combination of these datasets allows us to have an overview of the state of US air trafic",br(),
+                                      "Comparisons can be drawn between the two dashboards to reveal interesting results "
+                                    )
+                                    
+                           ),
+                           tabPanel("SNCF dashboard",
+                                    
+                                    fluidRow(column(width=1,
+                                                    wellPanel(
+                                                      radioButtons("choice", "Choose feature to aggregate on:",
+                                                                   c("Year" = "year",
+                                                                     "Departure Station" = "departure_station")
+                                                      )
+                                                    )
+                                                )
+                                                    
+                                    ),
+                                    column(width=8,
+                                           plotOutput('carried_bplot')
+                                           
+                                    ),
+                                    fluidRow(column(width=12),
+                                             plotOutput('multiple_bplot')
+                                    )
+                                  )
+                           )
 )
